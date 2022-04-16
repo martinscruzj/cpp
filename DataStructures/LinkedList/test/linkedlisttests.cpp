0b29fa42061052a2a998930770dd19d5349153c5
Martin <101901076+martinscruzj@users.noreply.github.com>
2022-04-15 18:05:19 -0700

    Add unit tests powered by google test (gtest)

--- DataStructures/LinkedList/test/linkedlisttests.cpp
+++ DataStructures/LinkedList/test/linkedlisttests.cpp
@@ -7,155 +7,185 @@ using namespace std;
 bool equal(int a, int b);
 bool lessthan(int a, int b);
 
-TEST(LinkedListTests, BasicAssertions) {
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
+class LinkedListTestFixture : public ::testing::Test {
+protected:
+   void SetUp() override {
+
+   }
+
+   void TearDown() override {
+
+   }
+   LinkedList list;
+};
+
+TEST_F(LinkedListTestFixture, SeveralTests) {
+   // Verify initial contents of just created list
+   std::stringstream ss;
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->NULL");
+   // List should not contain 100
+   EXPECT_FALSE(list.Find(100));
+   // Removing non-existing elements
+   list.Remove(100);
+   list.RemoveAll(100);
+   list.Remove(100, &equal);
+   list.RemoveAll(100, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->NULL");
+   // The list should be empty
+   EXPECT_TRUE(list.IsEmpty());
+   // Empty list and check
+   list.Empty();
+   EXPECT_TRUE(list.IsEmpty());
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->NULL");
+   // Add 1 to the list
+   list.Add(1);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->1->NULL");
+   EXPECT_FALSE(list.Find(100));
+   list.Remove(100);
+   list.RemoveAll(100);
+   list.Remove(100, &equal);
+   list.RemoveAll(100, &equal);
+   EXPECT_FALSE(list.IsEmpty());
+   EXPECT_TRUE(list.Find(1));
+   // Adding more elements to the list
+   list.Add(2); list.Add(3); list.Add(4); list.Add(5);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->5->4->3->2->1->NULL");
+   list.Remove(1);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->5->4->3->2->NULL");
+   list.Remove(5, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->3->2->NULL");
+   list.Remove(3, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->2->NULL");
+   list.Remove(2, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->NULL");
+   list.Remove(4, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->NULL");
+   // Adding elements to the list
+   list.Add(1); list.Add(2); list.Add(3); list.Add(4); list.Add(5);
+   list.RemoveAll(1, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->5->4->3->2->NULL");
+   list.RemoveAll(5);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->3->2->NULL");
+   list.RemoveAll(3, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->2->NULL");
+   list.RemoveAll(2);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->NULL");
+   list.RemoveAll(4, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->NULL");
+   // Adding items to the list
+   list.Add(1); list.Add(2); list.Add(3); list.Add(4); list.Add(5);
+   list.Add(1); list.Add(2); list.Add(3); list.Add(4); list.Add(5);
+   list.Add(1); list.Add(2); list.Add(3); list.Add(4); list.Add(5);
+   list.Add(1); list.Add(2); list.Add(3); list.Add(4); list.Add(5);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->5->4->3->2->1->"
+                                     "5->4->3->2->1->"
+                                     "5->4->3->2->1->"
+                                     "5->4->3->2->1->NULL");
+   list.RemoveAll(1, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->5->4->3->2->"
+                                     "5->4->3->2->"
+                                     "5->4->3->2->"
+                                     "5->4->3->2->NULL");
+   list.RemoveAll(5);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->3->2->"
+                                     "4->3->2->"
+                                     "4->3->2->"
+                                     "4->3->2->NULL");
+   list.RemoveAll(3, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->2->"
+                                     "4->2->"
+                                     "4->2->"
+                                     "4->2->NULL");
+   list.RemoveAll(2);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->"
+                                     "4->"
+                                     "4->"
+                                     "4->NULL");
+   list.RemoveAll(4, &equal);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->NULL");
+   // Add items to list
+   list.Add( 1); list.Add(-2); list.Add( 3); list.Add(-4); list.Add( 5);
+   list.Add(-1); list.Add( 2); list.Add(-3); list.Add( 4); list.Add(-5);
+   list.Add( 1); list.Add(-2); list.Add( 3); list.Add(-4); list.Add( 5);
+   list.Add(-1); list.Add( 2); list.Add(-3); list.Add( 4); list.Add(-5);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->-5->4->-3->2->-1->"
+                                     "5->-4->3->-2->1->"
+                                     "-5->4->-3->2->-1->"
+                                     "5->-4->3->-2->1->NULL");
+   // Remove first found negative item
+   list.Remove(0, &lessthan);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->-3->2->-1->"
+                                     "5->-4->3->-2->1->"
+                                     "-5->4->-3->2->-1->"
+                                     "5->-4->3->-2->1->NULL");
+   // Remove next negative item
+   list.Remove(0, &lessthan);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->2->-1->"
+                                     "5->-4->3->-2->1->"
+                                     "-5->4->-3->2->-1->"
+                                     "5->-4->3->-2->1->NULL");
+   // Remove all negative items
+   list.RemoveAll(0, &lessthan);
+   ss.str(""); // Clear stream
+   ss << list;
+   EXPECT_STREQ(ss.str().c_str(), "H->4->2->"
+                                     "5->3->1->"
+                                     "4->2->"
+                                     "5->3->1->NULL");
+   EXPECT_FALSE(list.IsEmpty());
+   EXPECT_FALSE(list.Find(100));
+   list.Remove(100);
+   list.RemoveAll(100, &equal);
+   EXPECT_FALSE(list.Find(100));
+   EXPECT_FALSE(list.IsEmpty());
 }
-
-/*int main(int argc, char** argv)
-{
-    LinkedList list;
-    testing::InitGoogleTest(&argc, argv);
-    cout << "Just created list contents: " << list << endl;
-    cout << "Is 100 contained in list? " << (list.Find(100)?"yes":"no") << endl;
-    cout << "Remove 100 from list" << endl;
-    list.Remove(100);
-    list.RemoveAll(100);
-    list.Remove(100, &equal);
-    list.RemoveAll(100, &equal);
-    cout << "Is list empty? " << (list.IsEmpty()?"yes":"no") << endl;
-    cout << "Empty list" << endl;
-    list.Empty();
-    cout << "Is list empty? " << (list.IsEmpty()?"yes":"no") << endl;
-    cout << "Contents after emptying list: " << list << endl;
-    cout << "Adding 1 to the list" << endl;
-    list.Add(1);
-    cout << "List contents: " << list << endl;
-    cout << "Is 100 contained in list? " << (list.Find(100)?"yes":"no") << endl;
-    cout << "Remove 100 from list" << endl;
-    list.Remove(100);
-    list.RemoveAll(100);
-    list.Remove(100, &equal);
-    list.RemoveAll(100, &equal);
-    cout << "Is list empty? " << (list.IsEmpty()?"yes":"no") << endl;
-    cout << "Is 1 contained in list? " << (list.Find(1)?"yes":"no") << endl;
-    cout << "Adding more elements to the list" << endl;
-    list.Add(2);
-    list.Add(3);
-    list.Add(4);
-    list.Add(5);
-    cout << "List contents: " << list << endl;
-    cout << "Remove 1 from list" << endl;
-    list.Remove(1);
-    cout << "List contents: " << list << endl;
-    cout << "Remove 5 from list" << endl;
-    list.Remove(5, &equal);
-    cout << "List contents: " << list << endl;
-    cout << "Remove 3 from list" << endl;
-    list.Remove(3);
-    cout << "List contents: " << list << endl;
-    cout << "Remove 2 from list" << endl;
-    list.Remove(2, &equal);
-    cout << "List contents: " << list << endl;
-    cout << "Remove 4 from list" << endl;
-    list.Remove(4);
-    cout << "List contents: " << list << endl;
-    cout << "Adding items to list" << endl;
-    list.Add(1);
-    list.Add(2);
-    list.Add(3);
-    list.Add(4);
-    list.Add(5);
-    cout << "Remove all 1s from list" << endl;
-    list.RemoveAll(1, &equal);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all 5s from list" << endl;
-    list.RemoveAll(5);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all 3s from list" << endl;
-    list.RemoveAll(3, &equal);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all 2s from list" << endl;
-    list.RemoveAll(2);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all 4s from list" << endl;
-    list.RemoveAll(4, &equal);
-    cout << "List contents: " << list << endl;
-    cout << "Adding items to list" << endl;
-    list.Add(1);
-    list.Add(2);
-    list.Add(3);
-    list.Add(4);
-    list.Add(5);
-    list.Add(1);
-    list.Add(2);
-    list.Add(3);
-    list.Add(4);
-    list.Add(5);
-    list.Add(1);
-    list.Add(2);
-    list.Add(3);
-    list.Add(4);
-    list.Add(5);
-    list.Add(1);
-    list.Add(2);
-    list.Add(3);
-    list.Add(4);
-    list.Add(5);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all 1s from list" << endl;
-    list.RemoveAll(1, &equal);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all 5s from list" << endl;
-    list.RemoveAll(5);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all 3s from list" << endl;
-    list.RemoveAll(3, &equal);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all 2s from list" << endl;
-    list.RemoveAll(2);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all 4s from list" << endl;
-    list.RemoveAll(4, &equal);
-    cout << "List contents: " << list << endl;
-    cout << "Adding items to list" << endl;
-    list.Add(1);
-    list.Add(-2);
-    list.Add(3);
-    list.Add(-4);
-    list.Add(5);
-    list.Add(-1);
-    list.Add(2);
-    list.Add(-3);
-    list.Add(4);
-    list.Add(-5);
-    list.Add(1);
-    list.Add(-2);
-    list.Add(3);
-    list.Add(-4);
-    list.Add(5);
-    list.Add(-1);
-    list.Add(2);
-    list.Add(-3);
-    list.Add(4);
-    list.Add(-5);
-    cout << "List contents: " << list << endl;
-    cout << "Remove first found negative item" << endl;
-    list.Remove(0, &lessthan);
-    cout << "List contents: " << list << endl;
-    cout << "Remove next negative item" << endl;
-    list.Remove(0, &lessthan);
-    cout << "List contents: " << list << endl;
-    cout << "Remove all negative items" << endl;
-    list.RemoveAll(0, &lessthan);
-    cout << "List contents: " << list << endl;
-    cout << "Is list empty? " << (list.IsEmpty()?"yes":"no") << endl;
-    cout << "Is 100 contained in list? " << (list.Find(100)?"yes":"no") << endl;
-    cout << "Remove 100 from list" << endl;
-    list.Remove(100);
-
-    return RUN_ALL_TESTS();
-}*/
 
 bool equal(int a, int b)
 {
